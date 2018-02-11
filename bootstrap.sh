1dc75d96a8000f6179e62104f5e14b8294e54b5a
Francesco Ciocchetti <primeroznl@gmail.com>
2018-02-11 14:05:49 +0000

    Update to working script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,21 @@
 
 # Install Ellipsis
 # XXX Change to github clone
+
+sudo pacman -Sy --noconfirm git
+
+eval `ssh-agent`
+ssh-add $HOME/.ssh/id_rsa
+
+rm -rf $HOME/.ellipsis.bak*
 curl https://ellipsis.sh | sh
 
 export PATH=$PATH:$HOME/.ellipsis/bin
 
 # Install some base ellipsis modules
-ellipsis install ssh://github.com/primeroz/dot-vim.git
-ellipsis install ssh://github.com/primeroz/dot-tmux.git
-ellipsis install ssh://github.com/primeroz/dot-git.git
-ellipsis install ssh://github.com/primeroz/dot-task.git
-ellipsis install ssh://github.com/primeroz/dot-zsh.git
+ellipsis install ssh://git@github.com/primeroz/dot-vim.git
+ellipsis install ssh://git@github.com/primeroz/dot-tmux.git
+ellipsis install ssh://git@github.com/primeroz/dot-git.git
+ellipsis install ssh://git@github.com/primeroz/dot-task.git
+ellipsis install ssh://git@github.com/primeroz/dot-zsh.git
+
